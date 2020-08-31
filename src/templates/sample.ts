8869d7e61a653182d711df4bd579690d41062090
lionel1702 <52881836+lionel1702@users.noreply.github.com>
2020-09-01 00:44:52 +0800

    First commit

--- src/templates/sample.ts
+++ src/templates/sample.ts
@@ -1,27 +1,20 @@
-import { v2 } from "@govtechsg/decentralized-renderer-react-components";
+import { Document } from "@govtechsg/decentralized-renderer-react-components";
 
-export interface CustomTemplateCertificate extends v2.OpenAttestationDocument {
+export interface CocTemplateCertificate extends Document {
   name: string;
-  institute: string;
-  foo?: {
-    title: string;
+  recipient: {
+    name: string;
   };
 }
 
-export const customTemplateCertificate: CustomTemplateCertificate = {
-  name: "John Doe",
-  institute: "Institute of John Doe",
-  issuers: [
-    {
-      name: "institute of blockchain"
-    }
-  ],
-  $template: {
-    name: "custom",
-    type: v2.TemplateType.EmbeddedRenderer,
-    url: "http://localhost:3000"
+export const cocTemplateCertificate: CocTemplateCertificate = {
+  name: "OpenAttestation Tutorial Certificate of Completion",
+  recipient: {
+    name: "Lim Gin Keat"
   },
-  foo: {
-    title: "Bar is awesome"
+  $template: {
+    name: "COC",
+    type: "EMBEDDED_RENDERER",
+    url: "http://localhost:3000"
   }
 };
